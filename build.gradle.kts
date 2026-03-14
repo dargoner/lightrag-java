d394ce20fc0943788dacf0cf48bfadfed0c16aee
chensheng <chensheng@finstone.com.cn>
2026-03-15 04:13:48 +0800

    build: align JUnit platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.1")
     testImplementation("org.assertj:assertj-core:3.27.3")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
